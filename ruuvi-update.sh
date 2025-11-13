c9e3751d57b75fbd10f2786aae7323f16cd857b8
Matias Nurmi <matias.nurmi@tuni.fi>
2025-11-13 14:39:41 +0200

    pass url as argument

--- ruuvi-update.sh
+++ ruuvi-update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# DO NOT EDIT
+URL="$1"
 MAC="$(cat /usr/share/mender/identity/mac)"
 ID="$(cat /usr/share/mender/identity/ruuvi-id)"
-
-# EDIT NEXT LINE AS NEEDED
-URL="https://github.com/ruuvi/ruuvi.firmware.c/releases/download/v3.30.2/ruuvitag_b_armgcc_ruuvifw_default_v3.30.2_dfu_app.zip"
 
 curl -H "X-Hook-Timeout: 600" "http://localhost:8080/update?url=$URL&mac=$MAC&id=$ID"
